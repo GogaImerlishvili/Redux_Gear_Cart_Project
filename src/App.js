078dadd5acc336a9500fc6b383b480d66ba5a32c
Goga <gogaimerlishvili1@gmail.com>
2022-04-18 00:53:38 +0400

    Added props

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import React from "react";
 import cartItems from "./cart-items";
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
+import { createStore } from "redux";
+
+const initialStore = {
+  count: 4,
+};
+
+function reducer(state, action) {
+  console.log({ state, action });
+  return state;
+}
+export const store = createStore(reducer, initialStore);
+
+// export const store = () => {
+//     let initialStore = {
+//       count: 2,
+//     };
+//   return createStore(reducer, initialStore);
+// };
 
 const App = () => {
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()} />
       <CartContainer cart={cartItems} />
     </main>
   );
