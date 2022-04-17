d4bd7c1af4aa5ac1702815caa0f31ed0f7636563
Goga <gogaimerlishvili1@gmail.com>
2022-04-18 01:34:54 +0400

    Added actions type

--- src/App.js
+++ src/App.js
@@ -5,14 +5,31 @@ import Navbar from "./components/Navbar";
 import { createStore } from "redux";
 
 const initialStore = {
-  count: 4,
+  count: 0,
+  name: "john",
 };
 
 function reducer(state, action) {
   console.log({ state, action });
+  if (action.type === "DECREASE") {
+    // state.count = state.count - 1;
+    return { ...state, count: state.count - 1 };
+  }
+  if (action.type === "INCREASE") {
+    // state.count = state.count - 1;
+    return { ...state, count: state.count + 1 };
+  }
+  if (action.type === "RESET") {
+    // state.count = state.count - 1;
+    return { ...state, count: 0 };
+  }
   return state;
 }
 export const store = createStore(reducer, initialStore);
+store.dispatch({ type: "DECREASE" });
+store.dispatch({ type: "INCREASE" });
+store.dispatch({ type: "INCREASE" });
+store.dispatch({ type: "RESET" });
 
 // export const store = () => {
 //     let initialStore = {
