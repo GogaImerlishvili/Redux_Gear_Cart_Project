d4a7dd807922376b289e1cbafc86c69f73b225ca
Goga <gogaimerlishvili1@gmail.com>
2022-04-18 22:57:25 +0400

    Cleared code

--- src/App.js
+++ src/App.js
@@ -7,13 +7,7 @@ import { createStore } from "redux";
 import reducer from "./reducer";
 import { Provider } from "react-redux";
 
-const initialStore = {
-  cart: cartItems,
-  total: 105,
-  amount: 5,
-};
-
-export const store = createStore(reducer, initialStore);
+export const store = createStore(reducer);
 
 const App = () => {
   return (
