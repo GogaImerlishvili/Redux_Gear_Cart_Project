527dd4ec15191927782b107f62b6eea779454976
Goga <gogaimerlishvili1@gmail.com>
2022-04-18 21:03:52 +0400

    Add more action

--- src/reducer.js
+++ src/reducer.js
@@ -1,22 +1,18 @@
-import { DECREASE, INCREASE, CLEAR_CART, REMOVE } from "./actions";
+import { DECREASE, INCREASE, CLEAR_CART, REMOVE, GET_TOTALS } from "./actions";
 import cartItems from "./cart-items";
 
 function reducer(state, action) {
   if (action.type === CLEAR_CART) {
     return { ...state, cart: [] };
   }
+
   if (action.type === DECREASE) {
-    let tempCart = [];
-    if (action.payload.amount === 1) {
-      tempCart = state.cart.filter((cartItems) => cartItems.id !== action);
-    } else {
-      tempCart = state.cart.map((cartItems) => {
-        if (cartItems.id === action.payload.id) {
-          cartItems = { ...cartItems, amount: cartItems.amount - 1 };
-        }
-        return cartItems;
-      });
-    }
+    let tempCart = state.cart.map((cartItems) => {
+      if (cartItems.id === action.payload.id) {
+        cartItems = { ...cartItems, amount: cartItems.amount - 1 };
+      }
+      return cartItems;
+    });
     return { ...state, cart: tempCart };
   }
   if (action.type === INCREASE) {
@@ -35,6 +31,24 @@ function reducer(state, action) {
         (cartItems) => cartItems.id !== action.payload.id
       ),
     };
+  }
+  if (action.type === GET_TOTALS) {
+    let { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+        const itemTotal = price * amount;
+
+        cartTotal.total += itemTotal;
+        cartTotal.amount += amount;
+        return cartTotal;
+      },
+      {
+        total: 0,
+        amount: 0,
+      }
+    );
+    total = parseFloat(total.toFixed(2));
+    return { ...state, total, amount };
   }
   return state;
 }
