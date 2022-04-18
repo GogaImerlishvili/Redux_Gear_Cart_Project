79f3b05e8ee4886dbb3db935103b06579c50b0e0
Goga <gogaimerlishvili1@gmail.com>
2022-04-18 23:14:29 +0400

    Fixed useEffect

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import { createStore } from "redux";
 import reducer from "./reducer";
 import { Provider } from "react-redux";
 
-export const store = createStore(reducer);
+export const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const App = () => {
   return (
